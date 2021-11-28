22118afa175390a70be28389a67a0622b01d2318
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2021-11-28 10:30:09 -0300

    Initial commit

---
+++ Novo1.pas
@@ -0,0 +1,26 @@
+Program Pzim ;
+var
+
+v,cont:real;
+
+ Begin
+ 
+  while cont<=5 and v<v do
+  
+  
+  
+ begin
+ writeln('De quanto é o valor?');
+ readln(v);
+  cont:=cont+1
+  writeln(v)
+  end;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ End.
---
+++ pascal1.pas
@@ -0,0 +1,18 @@
+Program Pzim ;
+var
+i:integer;
+e,vm:real;
+begin
+
+i:= i+1;
+while(i<=5) do
+ Begin
+ writeln('De quanto é o valor?');
+ readln(e);
+ vm:=(e<1000)
+ writeln(vm);
+ end;
+ 
+ 
+  
+ End.
